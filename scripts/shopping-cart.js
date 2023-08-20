d52a1dfa074229fc21d6b360f8baa18786ffbac1
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-08-21 00:02:07 +0600

    total price and discount added

--- scripts/shopping-cart.js
+++ scripts/shopping-cart.js
@@ -1,5 +1,5 @@
 let productCount = 0;
-document.getElementById('kid-one').addEventListener('click', function(){
+document.getElementById('kid-one').addEventListener('click', function () {
     const productTitle = document.getElementById('k-title-one').innerText;
 
     // cart top
@@ -9,14 +9,53 @@ document.getElementById('kid-one').addEventListener('click', function(){
     const cardTop = document.getElementById('products');
     cardTop.appendChild(p);
 
-    // total price
-    const productPriceText = document.getElementById('k-price-one').innerText;
-    const productPrice = parseFloat(productPriceText);
-
     const totalPriceElement = document.getElementById('total-price');
-    const totalPriceText = totalPriceElement.innerText;
-    const previousTotalPrice = parseFloat(totalPriceText);
-    let totalPrice =  (previousTotalPrice + productPrice);
-
+    let totalPrice = calculateTotalPrice('k-price-one', 'total-price');
     totalPriceElement.innerText = totalPrice;
+    if(totalPrice >= 200){
+        document.getElementById('btn-apply-coupon').removeAttribute('disabled');
+    }
 })
+
+// document.getElementById('btn-apply-coupon').addEventListener('click', function(){   
+//     let productPriceText = document.getElementById('k-price-one').innerText;
+//     let productPrice = parseFloat(productPriceText);
+
+//     let totalPrice = calculateTotalPrice('k-price-one', 'total-price');
+//     let discount = 0;
+//     const couponCode = document.getElementById('coupon-code').value;
+//     if(couponCode === 'SELL200'){
+//         discount = (totalPrice - productPrice) * 0.2;
+//     }
+//     const discountElement = document.getElementById('discount');
+//     discountElement.innerText = discount.toFixed(2);     
+// })
+
+document.getElementById('btn-apply-coupon').addEventListener('click', function(){
+    handleDiscount('k-price-one', 'total-price', 'coupon-code', 'SELL200', 'discount');
+})
+
+function calculateTotalPrice(priceId, totalPriceId) {
+    // total price
+    let productPriceText = document.getElementById(priceId).innerText;
+    let productPrice = parseFloat(productPriceText);
+
+    let totalPriceElement = document.getElementById(totalPriceId).innerText;
+    let previousTotalPrice = parseFloat(totalPriceElement);
+    previousTotalPrice += productPrice;
+    return previousTotalPrice ;
+}
+
+function handleDiscount(productPriceId, totalPriceId, couponCodeId, coupon, discountId){ 
+        let productPriceText = document.getElementById(productPriceId).innerText;
+        let productPrice = parseFloat(productPriceText);
+    
+        let totalPrice = calculateTotalPrice(productPriceId, totalPriceId);
+        let discount = 0;
+        const couponCode = document.getElementById(couponCodeId).value;
+        if(couponCode === coupon){
+            discount = (totalPrice - productPrice) * 0.2;
+        }
+        const discountElement = document.getElementById(discountId);
+        discountElement.innerText = discount.toFixed(2); 
+}
